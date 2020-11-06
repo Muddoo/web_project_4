95f4a0f66da1bc16e79548782a6539833d190eb3
Moddoo <>
2020-11-06 00:39:50 -0600

    ready to review

--- pages/index.js
+++ pages/index.js
@@ -4,42 +4,16 @@ const txtName = document.querySelector('.profile__name');
 const txtInfo = document.querySelector('.profile__text');
 const cards = document.querySelector('.cards');
 const templateCard = document.querySelector('#template-card').content;
-const templateForm = document.querySelector('#template-form').content;
-const templateFigure = document.querySelector('#template-figure').content;
-const formElement = templateForm.cloneNode(true);
-const addElement = templateForm.cloneNode(true);
-const figureElement = templateFigure.cloneNode(true);
-let formBox;
-let addBox;
-
-formElement.querySelector('.popup').id = 'form';
-addElement.querySelector('.popup').id = 'add';
-
-function handleClick(e,closeIcon,element,submitObject,removeListener) {
-  if(submitObject && e.target === submitObject.button) {
-    e.preventDefault();
-    submitObject.button.disabled = true;
-    for(key in submitObject) {
-      if(Array.isArray(submitObject[key])) submitObject[key][0][submitObject[key][2]] = submitObject[key][1];
-      if(typeof submitObject[key] === 'function') submitObject[key]();
-    };
-  };
-  if(e.target === element || e.target === closeIcon || e.target === submitObject.button) {
-    element.classList.remove('visible');
-    if(removeListener) element.removeEventListener('click', removeListener);
-  } 
-};
 
 function showImage(link,name) {
-    document.querySelector('script').before(figureElement);
     const popupFigure = document.querySelector('.popup-figure');
     const figureImage = popupFigure.querySelector('.popup-figure__image');
     const figureCaption = popupFigure.querySelector('.popup-figure__caption');
-    const closeButton = popupFigure.querySelector('.popup-figure__icon');
-    setTimeout(() => popupFigure.classList.add('visible'), 20);
+    const closeButton = popupFigure.querySelector('.popup-figure__close');
+    popupFigure.classList.add('visible');
     figureImage.setAttribute('src', link);
     figureCaption.textContent = name;
-    popupFigure.addEventListener('click', e => handleClick(e,closeButton,popupFigure));
+    popupFigure.addEventListener('click', e => (e.target === popupFigure || e.target === closeButton) && popupFigure.classList.remove('visible'));
 }
 
 function createCard(link, name, insert = 'append') {
@@ -51,97 +25,86 @@ function createCard(link, name, insert = 'append') {
   cardImage.setAttribute('src', link);
   cardText.textContent = name;
   cardImage.addEventListener('click', e => showImage(link,name));
-  cardLike.addEventListener('click', e => e.target.src.includes('black') ? e.target.src = './images/Vectorheart.jpg' : e.target.src = './images/blackHeart.svg');
+  cardLike.addEventListener('click', e => e.target.classList.toggle('card__icon-heart_black'));
   cardDelete.addEventListener('click', e => e.target.closest('.card').remove())
-  if(insert !== 'append') return cards.prepend(cardElement);
-  cards.append(cardElement);
-}
+  cards[insert](cardElement);
+};
 
-(function initialCards() {
-    const initialCards = [
-        {
-          name: "Yosemite Valley",
-          link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-        },
-        {
-          name: "Lake Louise",
-          link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-        },
-        {
-          name: "Bald Mountains",
-          link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-        },
-        {
-          name: "Latemar",
-          link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-        },
-        {
-          name: "Vanoise National Park",
-          link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-        },
-        {
-          name: "Lago di Braies",
-          link: "https://code.s3.yandex.net/web-code/lago.jpg"
-        }
-      ]; 
-      for(initialCard of initialCards)  createCard(initialCard.link, initialCard.name);
-})();
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ]; 
+for(initialCard of initialCards)  createCard(initialCard.link, initialCard.name);
 
 function editForm(e) {
     e.stopPropagation();
-    document.querySelector('script').before(formElement);
-    formBox = document.querySelector('#form');
-    const formTitle = formBox.querySelector('.popup__header');
-    const formButton  = formBox.querySelector('.popup__button');
-    const closeButton = formBox.querySelector('.popup__icon');
+    const formBox = document.querySelector('#form');
+    const formButton  = formBox.querySelector('.popup__submit');
+    const closeButton = formBox.querySelector('.popup__close');
     const inputName = formBox.querySelector('#name');
     const inputInfo = formBox.querySelector('#about');
-    formTitle.textContent = 'Edit profile';
-    formButton.textContent = 'Save';
     inputName.value = txtName.textContent;
     inputInfo.value = txtInfo.textContent;
-    setTimeout(() => {
-      formBox.classList.add('visible');
-      inputName.focus();
-    }, 22);
+    formBox.classList.add('visible');
+    inputName.focus();
     formBox.addEventListener('click', function form(e) {
-      formObj = {
-        button: formButton,
-        nameValue: [txtName, inputName.value, 'textContent'],
-        infoValue: [txtInfo, inputInfo.value, 'textContent'],
+      if(e.target === formButton) {
+        e.preventDefault();
+        formButton.disabled = true;
+        txtName.textContent = inputName.value;
+        txtInfo.textContent = inputInfo.value;
       };
-      handleClick(e,closeButton,this,formObj,form);
+      if(e.target === formButton || e.target === formBox || e.target === closeButton) {
+        formBox.classList.remove('visible');
+        formBox.removeEventListener('click', form);
+      };
     });
     formButton.disabled = false;
 };
 
 function addForm(e) {
     e.stopPropagation();
-    document.querySelector('script').before(addElement);
-    addBox = document.querySelector('#add');
-    const formTitle = addBox.querySelector('.popup__header');
-    const formButton  = addBox.querySelector('.popup__button');
-    const closeButton = addBox.querySelector('.popup__icon');
+    const addBox = document.querySelector('#add');
+    const formButton  = addBox.querySelector('.popup__submit');
+    const closeButton = addBox.querySelector('.popup__close');
     const inputName = addBox.querySelector('#name');
-    const inputLink = addBox.querySelector('#about');
-    formTitle.textContent = 'New place';
-    formButton.textContent = 'Create';
-    inputName.placeholder = 'Title';
-    inputLink.placeholder = 'Image link';
-    setTimeout(() => {
-      addBox.classList.add('visible');
-      inputName.focus();
-    }, 22);
+    const inputLink = addBox.querySelector('#image');
+    addBox.classList.add('visible');
+    inputName.focus();
     addBox.addEventListener('click', function add(e) {
-      createObj = {
-        button: formButton,
-        create() {
-          createCard(inputLink.value, inputName.value, 'prepend');
-        },
-        nameValue: [inputName, '', 'value'],
-        linkValue: [inputLink, '', 'value'],
+      if(e.target === formButton) {
+        e.preventDefault();
+        formButton.disabled = true;
+        createCard(inputLink.value, inputName.value, 'prepend');
+        inputName.value = '';
+        inputLink.value = '';
       }
-      handleClick(e,closeButton,this,createObj,add);
+      if(e.target === formButton || e.target === addBox || e.target === closeButton) {
+        addBox.classList.remove('visible');
+        addBox.removeEventListener('click', add);
+      };
     });
     formButton.disabled = false;
 };

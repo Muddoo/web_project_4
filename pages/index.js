93860fcdf3ad155277d1a06440b457b67afbad4c
Moddoo <>
2020-11-07 07:44:12 -0600

    final touch

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ const txtName = document.querySelector('.profile__name');
 const txtInfo = document.querySelector('.profile__text');
 const cards = document.querySelector('.cards');
 const templateCard = document.querySelector('#template-card').content;
+const popupFigure = document.querySelector('.popup-figure');
+const formBox = document.querySelector('#form');
+const addBox = document.querySelector('#add');
 
 function showImage(link,name) {
-    const popupFigure = document.querySelector('.popup-figure');
     const figureImage = popupFigure.querySelector('.popup-figure__image');
     const figureCaption = popupFigure.querySelector('.popup-figure__caption');
     const closeButton = popupFigure.querySelector('.popup-figure__close');
@@ -16,7 +18,7 @@ function showImage(link,name) {
     popupFigure.addEventListener('click', e => (e.target === popupFigure || e.target === closeButton) && popupFigure.classList.remove('visible'));
 }
 
-function createCard(link, name, insert = 'append') {
+function createCard(link, name) {
   const cardElement = templateCard.cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardText  = cardElement.querySelector('.card__text');
@@ -27,7 +29,7 @@ function createCard(link, name, insert = 'append') {
   cardImage.addEventListener('click', e => showImage(link,name));
   cardLike.addEventListener('click', e => e.target.classList.toggle('card__icon-heart_black'));
   cardDelete.addEventListener('click', e => e.target.closest('.card').remove())
-  cards[insert](cardElement);
+  cards.prepend(cardElement);
 };
 
 const initialCards = [
@@ -56,11 +58,10 @@ const initialCards = [
       link: "https://code.s3.yandex.net/web-code/lago.jpg"
     }
   ]; 
-for(initialCard of initialCards)  createCard(initialCard.link, initialCard.name);
+for(const initialCard of initialCards)  createCard(initialCard.link, initialCard.name);
 
 function editForm(e) {
     e.stopPropagation();
-    const formBox = document.querySelector('#form');
     const formButton  = formBox.querySelector('.popup__submit');
     const closeButton = formBox.querySelector('.popup__close');
     const inputName = formBox.querySelector('#name');
@@ -69,14 +70,14 @@ function editForm(e) {
     inputInfo.value = txtInfo.textContent;
     formBox.classList.add('visible');
     inputName.focus();
-    formBox.addEventListener('click', function form(e) {
-      if(e.target === formButton) {
-        e.preventDefault();
+    formBox.addEventListener('click', function form(ev) {
+      if(ev.target === formButton) {
+        ev.preventDefault();
         formButton.disabled = true;
         txtName.textContent = inputName.value;
         txtInfo.textContent = inputInfo.value;
       };
-      if(e.target === formButton || e.target === formBox || e.target === closeButton) {
+      if(ev.target === formButton || ev.target === formBox || ev.target === closeButton) {
         formBox.classList.remove('visible');
         formBox.removeEventListener('click', form);
       };
@@ -86,22 +87,21 @@ function editForm(e) {
 
 function addForm(e) {
     e.stopPropagation();
-    const addBox = document.querySelector('#add');
     const formButton  = addBox.querySelector('.popup__submit');
     const closeButton = addBox.querySelector('.popup__close');
     const inputName = addBox.querySelector('#img-name');
     const inputLink = addBox.querySelector('#image');
     addBox.classList.add('visible');
     inputName.focus();
-    addBox.addEventListener('click', function add(e) {
-      if(e.target === formButton) {
-        e.preventDefault();
+    addBox.addEventListener('click', function add(ev) {
+      if(ev.target === formButton) {
+        ev.preventDefault();
         formButton.disabled = true;
         createCard(inputLink.value, inputName.value, 'prepend');
         inputName.value = '';
         inputLink.value = '';
       }
-      if(e.target === formButton || e.target === addBox || e.target === closeButton) {
+      if(ev.target === formButton || ev.target === addBox || ev.target === closeButton) {
         addBox.classList.remove('visible');
         addBox.removeEventListener('click', add);
       };

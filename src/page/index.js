32badf9731ae5e23e17b3d993062b8fb5604870d
Moddoo <>
2020-12-24 10:42:38 -0600

    refactoring: webpack

--- src/page/index.js
+++ src/page/index.js
@@ -1,5 +1,35 @@
 import './index.css'
-import {initialCards,newSection,formValidator,handleEditButtonClick,handleAddButtonClick} from '../utils/constants.js'
+import Card from '../components/Card.js'
+import FormValidator from '../components/FormValidator.js'
+import Section from '../components/Section.js'
+import PopupWithForm from '../components/PopupWithForm.js'
+import PopupWithImage from '../components/PopupWithImage.js'
+import UserInfo from '../components/UserInfo.js'
+import {initialCards} from '../utils/constants.js'
+
+const imagePopupObject = new PopupWithImage('.popup_figure');
+const handleCardClick = (link,name) => imagePopupObject.open(link,name);
+const renderer = item => new Card({item,handleCardClick},'.template-card').generateCard();
+const newSection = items => new Section({items, renderer}, '.cards').add();
+const userInfo = new UserInfo(['.profile__name','.profile__text']);
+const profileFormSubmit = ([name,info]) => userInfo.setUserInfo(name,info);
+const cardFormSubmit = ([name,link]) => newSection([{name,link}]);
+const formValidator = form => new FormValidator({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'inactive',
+  inputErrorClass: 'popup__field_border_red',
+  errorClass: 'popup__error_visible'
+},form).enableValidation();
+const handleEditButtonClick = new PopupWithForm('.popup_profile',{
+  info: userInfo,
+  submit: profileFormSubmit
+});
+const handleAddButtonClick = new PopupWithForm('.popup_card', {
+  submit: cardFormSubmit
+});
+
 newSection(initialCards);
 document.querySelectorAll('.popup__form').forEach(form => formValidator(form));
 document.querySelector('.profile__edit-button').addEventListener('click', () => handleEditButtonClick.open());
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,33 +1,4 @@
-import Card from '../components/Card.js'
-import FormValidator from '../components/FormValidator.js'
-import Section from '../components/Section.js'
-import PopupWithForm from '../components/PopupWithForm.js'
-import PopupWithImage from '../components/PopupWithImage.js'
-import UserInfo from '../components/UserInfo.js'
-
-const imagePopupObject = new PopupWithImage('.popup_figure');
-const handleCardClick = (link,name) => imagePopupObject.open(link,name);
-const renderer = item => new Card({item,handleCardClick},'.template-card').generateCard();
-const newSection = items => new Section({items, renderer}, '.cards').add();
-const userInfo = new UserInfo(['.profile__name','.profile__text']);
-const profileFormSubmit = ([name,info]) => userInfo.setUserInfo(name,info);
-const cardFormSubmit = ([name,link]) => newSection([{name,link}]);
-const formValidator = form => new FormValidator({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__field',
-  submitButtonSelector: '.popup__submit',
-  inactiveButtonClass: 'inactive',
-  inputErrorClass: 'popup__field_border_red',
-  errorClass: 'popup__error_visible'
-},form).enableValidation();
-const handleEditButtonClick = new PopupWithForm('.popup_profile',{
-  info: userInfo,
-  submit: profileFormSubmit
-});
-const handleAddButtonClick = new PopupWithForm('.popup_card', {
-  submit: cardFormSubmit
-});
-const initialCards = [
+export const initialCards = [
     {
       name: "Yosemite Valley",
       link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
@@ -53,5 +24,3 @@ const initialCards = [
       link: "https://code.s3.yandex.net/web-code/lago.jpg"
     }
 ];
-
-export {initialCards,newSection,userInfo,formValidator,handleEditButtonClick,handleAddButtonClick}
